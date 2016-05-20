db4e68b4b9124ccd42f9d77c7da7a0baea2597a6
moochi1 <seyedmohsenalavi@yahoo.com>
2016-05-20 10:20:30 -0400

    Update frt.py

--- frt.py
+++ frt.py
@@ -1,1 +1,50 @@
 
+import MySQLdb
+
+from MySQLdb import cursors
+
+
+
+
+db= MySQLdb.connect(host="localhost",user="root",passwd="root",db="test")
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT * FROM marks")
+
+# print all the first cell of all the rows
+
+result=cur.fetchall()
+print (result)
+
+
+
+
+
+
+
+
+
+
+
+
+current = 0
+
+diff=0
+
+for row in result:
+	i=db.cursor()
+	previous=current
+
+	current= row[1]
+
+	difference=int(current-previous)
+	print (type(row[0]))
+	print (type(difference))
+	print ("UPDATE marks SET diff=" +str(difference) + " where name = \'"+ row[0] + "\'")
+	
+	i.execute("UPDATE marks SET diff=" +str(difference) + " where name = \'"+ row[0] + "\'")
+	db.commit()
+
+
+db.close()
